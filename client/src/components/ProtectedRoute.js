502da85543bb7e8d58b1db05d5cae5977453aaa0
Ayushi Singh <aayu16patwal@gmail.com>
2023-11-18 00:52:09 +0530

    Added redux files

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,8 +1,12 @@
 import React, { children, useEffect, useState } from "react";
 import axios from "axios";
+import { message } from "antd";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { SetUser } from "../redux/usersSlice";
 
 function ProtectedRoute({ children }) {
+  const dispatch=useDispatch();
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const validateToken = async () => {
@@ -18,12 +22,19 @@ function ProtectedRoute({ children }) {
       );
       if (response.data.success) {
         setLoading(false);
+        dispatch(SetUser(response.data.data));
       } else {
         setLoading(false);
+        localStorage.removeItem("token");
+        message.error(response.data.message);
         navigate("/login");
+
       }
     } catch (error) {
+      localStorage.removeItem("token");
+      message.error(error.message);
       setLoading(false);
+     
       navigate("/login");
     }
   };
