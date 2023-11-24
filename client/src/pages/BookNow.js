1ee3cab42fa640956f24c0669c72c7e2efacc009
Ayushi Singh <aayu16patwal@gmail.com>
2023-11-24 17:17:38 +0530

    Booking Payment via Stripe

--- client/src/pages/BookNow.js
+++ client/src/pages/BookNow.js
@@ -5,6 +5,7 @@ import { HideLoading, ShowLoading } from "../redux/alertsSlice";
 import { axiosInstance } from "../helpers/axiosInstance";
 import { useParams } from "react-router-dom";
 import SeatSelection from "../components/SeatSelection";
+import StripeCheckout from "react-stripe-checkout";
 function BookNow() {
   const [selectedSeats, setSelectedSeats] = useState([]); //object created
   const params = useParams();
@@ -28,26 +29,27 @@ function BookNow() {
     }
   };
 
-  const bookNow= async()=>{
-    try{
+  const bookNow = async () => {
+    try {
       dispatch(ShowLoading());
-      const response=await axiosInstance.post("/api/bookings/book-seat",{
+      const response = await axiosInstance.post("/api/bookings/book-seat", {
         bus: bus._id,
-        seats:selectedSeats,
+        seats: selectedSeats,
       });
       dispatch(HideLoading());
-      if(response.data.success){
+      if (response.data.success) {
         message.success(response.data.message);
-      }
-      else{
+      } else {
         message.error(response.data.message);
       }
-    }
-    catch(error)
-    {
+    } catch (error) {
       dispatch(HideLoading());
       message.error(error.message);
     }
+  };
+
+  const onToken=(token)=>{
+    console.log(token)
   }
 
   useEffect(() => {
@@ -59,35 +61,62 @@ function BookNow() {
       {bus && (
         <Row className="mt-3" gutter={20}>
           <Col lg={12} xs={24} sm={24}>
-            <h1 className="text-2xl text-secondary">
-            {bus.name}
-            </h1>
+            <h1 className="text-2xl text-secondary">{bus.name}</h1>
             <h1 className="text-md">
               {bus.from}-{bus.to}{" "}
             </h1>
             <hr />
 
             <div className="flex flex-col gap-1">
-                <h1 className="text-lg"> <b>Journey Date</b> : {bus.journeyDate} </h1>
-                <h1 className="text-lg"> <b>Fare</b> :${bus.fare} /-</h1>
-                <h1 className="text-lg"> <b>Departure Time</b> : {bus.departure} </h1>
-                <h1 className="text-lg"> <b>Arrival Time</b> : {bus.arrival} </h1>
-                <h1 className="text-lg"> <b>Capacity</b> : {bus.capacity} </h1>
-                <h1 className="text-lg"> <b>Seats Left</b> : {bus.capacity-bus.seatsBooked.length} </h1>
+              <h1 className="text-lg">
+                {" "}
+                <b>Journey Date</b> : {bus.journeyDate}{" "}
+              </h1>
+              <h1 className="text-lg">
+                {" "}
+                <b>Fare</b> :${bus.fare} /-
+              </h1>
+              <h1 className="text-lg">
+                {" "}
+                <b>Departure Time</b> : {bus.departure}{" "}
+              </h1>
+              <h1 className="text-lg">
+                {" "}
+                <b>Arrival Time</b> : {bus.arrival}{" "}
+              </h1>
+              <h1 className="text-lg">
+                {" "}
+                <b>Capacity</b> : {bus.capacity}{" "}
+              </h1>
+              <h1 className="text-lg">
+                {" "}
+                <b>Seats Left</b> : {bus.capacity - bus.seatsBooked.length}{" "}
+              </h1>
             </div>
-            <hr/>
+            <hr />
             <div className="flex flex-col gap-2">
-            <h1 className="text-2xl">
-              Selected Seats : {selectedSeats.join(",")}
-            </h1>
-            <h1 className="text-2xl mt-2">Fare : $ {bus.fare* selectedSeats.length} /- </h1>
-            <hr/>
-            <button 
-            className={`btn btn-primary ${selectedSeats.length===0 &&  "disabled-btn"}`}  onClick={bookNow}
-            disabled={selectedSeats.length===0}
-            >Book Now</button>
-            </div>
+              <h1 className="text-2xl">
+                Selected Seats : {selectedSeats.join(",")}
+              </h1>
+              <h1 className="text-2xl mt-2">
+                Fare : $ {bus.fare * selectedSeats.length} /-{" "}
+              </h1>
+              <hr />
 
+              <StripeCheckout
+                token={onToken}
+                stripeKey="pk_test_51OFxHtSEV8Jywm5zP1nBDRDU9Wrh6rLT3X4nZjKIth7V38KKIwqjg1Mta0E5k9IpNW8Hh0mtaqlbn2FzPxyoeMWi00fWfKbe3t"
+              >
+                <button
+                  className={`btn btn-primary ${
+                    selectedSeats.length === 0 && "disabled-btn"
+                  }`}
+                  disabled={selectedSeats.length === 0}
+                >
+                  Book Now
+                </button>
+              </StripeCheckout>
+            </div>
           </Col>
           <Col lg={12} xs={24} sm={24}>
             <SeatSelection
