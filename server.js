104371dff65fa8faade3427dc3f1387eb2a60a5e
Ayushi Singh <aayu16patwal@gmail.com>
2024-02-04 15:37:05 +0530

    Deployment Changes

--- server.js
+++ server.js
@@ -11,10 +11,21 @@ app.use(express.json());
 
 const usersRoute = require("./routes/usersRoute");
 const busesRoute = require("./routes/busesRoute");
-const bookingsRoute=require("./routes/bookingsRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
 
 app.use("/api/users", usersRoute);
 app.use("/api/buses", busesRoute);
-app.use("/api/bookings",bookingsRoute);
+app.use("/api/bookings", bookingsRoute);
+
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Node server Listening on port ${port}`));
